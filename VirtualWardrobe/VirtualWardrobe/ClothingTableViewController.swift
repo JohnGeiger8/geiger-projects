8ca911dbf593b649b1d694e43f39d39e57ad0361
John Geiger <jqg5562@psu.edu>
2019-11-02 13:35:14 -0400

    Add search bar and 2 new items

--- VirtualWardrobe/VirtualWardrobe/ClothingTableViewController.swift
+++ VirtualWardrobe/VirtualWardrobe/ClothingTableViewController.swift
@@ -8,15 +8,22 @@
 
 import UIKit
 
-class ClothingTableViewController: UITableViewController {
+class ClothingTableViewController: UITableViewController, UISearchResultsUpdating, UISearchControllerDelegate {
+    
     
     let wardrobeModel = WardrobeModel.sharedinstance
+    let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-//        tableView.estimatedRowHeight = 500
-//        tableView.rowHeight = UITableView.automaticDimension
+        searchController.searchResultsUpdater = self
+        searchController.delegate = self
+        
+        searchController.searchBar.showsScopeBar = true
+        searchController.searchBar.scopeButtonTitles = ["By name", "By type", "By color", "By store"]
+        
+        tableView.tableHeaderView = searchController.searchBar
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -57,15 +64,15 @@ class ClothingTableViewController: UITableViewController {
         return 300  // FIXME: Need to make this more dynamic
     }
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -75,31 +82,10 @@ class ClothingTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    // MARK:- Search Results Updater
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        // IMPLEMENT
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- VirtualWardrobe/VirtualWardrobe/WardrobeModel.swift
+++ VirtualWardrobe/VirtualWardrobe/WardrobeModel.swift
@@ -12,12 +12,14 @@ import MapKit
 struct Clothing {
     var name : String
     var type : String
+    var subType : String // E.g. type = "shirt" subtype = "long-sleeve"
     var colors : [String]
     var seasons : [String]
     var price : Double
     var storeName : String
     var storeLocation : CLLocationCoordinate2D
     var imageName : String
+    var dateOfPurchase : Date
 }
 
 
@@ -25,6 +27,10 @@ class WardrobeModel {
     
     // Have all files using model use the same instance
     static let sharedinstance = WardrobeModel()
+    
+    let clothingNames = ["Carson Wentz Eagles Jersey", "Sperry Docksider Shoes", "Red and Blue Flannel"]
+    let clothingImageNames = ["Wentz Jersey", "Sperry Shoes", "Red and Blue Flannel"]
+    let clothingStoreNames = ["NFLShop.com", "Goodwill", "REI"]
     
     // Number of sections.  This should change depending on filter user chooses
     var numberOfSections = 1
@@ -38,21 +44,21 @@ class WardrobeModel {
     // Use switch-case statement to give number of rows for the section number depending on the filter chosen by the user
     func numberOfClothes(forSection section: Int) -> Int {
         
-        return 1
+        return clothingStoreNames.count
     }
     
     func clothingNameFor(indexPath: IndexPath) -> String {
         
-        return "Carson Wentz Eagles Jersey"
+        return clothingNames[indexPath.row]
     }
     
     func clothingImageNameFor(indexPath: IndexPath) -> String {
         
-        return "Wentz Jersey"
+        return clothingImageNames[indexPath.row]
     }
     
     func clothingStoreNameFor(indexPath: IndexPath) -> String {
         
-        return "NFLShop.com"
+        return clothingStoreNames[indexPath.row]
     }
 }
