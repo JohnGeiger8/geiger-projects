c93b022c34e3f631268685dc1d9317bbb3ed958c
John Geiger <jqg5562@psu.edu>
2020-02-09 16:55:07 -0500

    Implement move function
    Function updates the bodyPositions list of the Snake object depending on the direction

--- Snake/SnakeModel.py
+++ Snake/SnakeModel.py
@@ -7,29 +7,49 @@ Snake Model
 
 """
 
+class Direction:
+    
+    Left = -1
+    Up = -1
+    Right = 1
+    Down = 1
+
 class Snake:
     
     bodyPositions = []
+    direction = Direction.Down
     
     def __init__(self):
         
-        print("I'm a snake!")
+        self.bodyPositions = [(0,0)]
         
-    def move(self, direction):
         
-        if direction == Direction.Left:
-            print("left")
-        if direction == Direction.Up:
-            print("Up")
-        if direction == Direction.Right:
-            print("Right")
-        if direction == Direction.Down:
-            print("Down")
+    def setBodyPositions(self, positions):
+        
+        self.bodyPositions = positions
+        
+        
+    def move(self):
+        
+        self.bodyPositions.pop(0)
+
+        headPosition = self.bodyPositions[len(self.bodyPositions) - 1]
+
+        if self.direction == Direction.Left or self.direction == Direction.Right:
+            """ Horizontal move """
+            
+            # New position changes the column by 1 or -1 for right or left movement
+            newPosition = (headPosition[0], headPosition[1] + self.direction)
+            
+            self.bodyPositions.append(newPosition)
+            
+            
+        elif self.direction == Direction.Up or self.direction == Direction.Down:
+            """ Vertical move """
+            
+            # New position changes the row by 1 or -1 for down or up movement
+            newPosition = (headPosition[0] + self.direction, headPosition[1])
+            
+            self.bodyPositions.append(newPosition)
+
                 
-     
-class Direction:
-    
-    Left = 0
-    Up = 1
-    Right = 2
-    Down = 3
