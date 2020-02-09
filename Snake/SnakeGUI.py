7928be9eab3964e0703e68ce38bc1996ca6409d1
John Geiger <jqg5562@psu.edu>
2020-02-09 16:04:07 -0500

    Add SnakeFrame class which will be the actual board where the snake moves

--- Snake/SnakeGUI.py
+++ Snake/SnakeGUI.py
@@ -8,15 +8,57 @@ Created on Sat Feb  8 15:32:20 2020
 Snake GUI
 
 """
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFrame
+from PyQt5.QtCore import Qt, QTimer, pyqtSignal
+from SnakeModel import Snake, Direction
+
+
+class SnakeWindow(QMainWindow):
+    
+    gameTimer = QTimer()
+    
+    def __init__(self):
+        
+        super().__init__()
+        self.initGame()
+        
+    def initGame(self):
+        
+        self.gameFrame = SnakeFrame()
+        self.gameFrame.startGame()
+                
+        self.setGeometry(300, 300, 500, 500)
+        self.setWindowTitle('Snake')    
+        self.show()
+        
+class SnakeFrame(QFrame):
+    
+    gameLoopTime = 30
+    
+    def __init__(self):
+        
+        super().__init__()
+        self.initFrame()
+        
+    def initFrame(self):
+        
+        self.gameTimer = QTimer()
+        self.snake = Snake()
+
+    def startGame(self):
+        
+        self.gameTimer.timeout.connect(self.moveSnake)
+        self.gameTimer.start(self.gameLoopTime)
+       
+    
+    def moveSnake(self):
+        """ Called every time timer times out """
+        
+        print("Timer")
 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple')
-    w.show()
-    
+    window = SnakeWindow()
     sys.exit(app.exec_())
