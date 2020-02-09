8b04073df853269bac9145c670af5d2d5fe279de
John Geiger <jqg5562@psu.edu>
2020-02-09 17:58:04 -0500

    Further develop SnakeFrame
    Add paint event for drawing the snake in its initial position.
    Draws a rectangle for each snake position square.  Window is now
    sized based on the frames specified squareSize and width and height.

--- Snake/SnakeGUI.py
+++ Snake/SnakeGUI.py
@@ -11,13 +11,13 @@ Snake GUI
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QFrame
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal
+from PyQt5.QtGui import QPainter, QColor, QPen, QBrush
 from SnakeModel import Snake, Direction
 
 
 class SnakeWindow(QMainWindow):
-    
-    gameTimer = QTimer()
-    
+    """ Main window that starts the game """
+        
     def __init__(self):
         
         super().__init__()
@@ -27,24 +27,39 @@ class SnakeWindow(QMainWindow):
         
         self.gameFrame = SnakeFrame()
         self.gameFrame.startGame()
+        
+        self.setCentralWidget(self.gameFrame)
                 
-        self.setGeometry(300, 300, 500, 500)
+        self.setGeometry(300, 150, self.gameFrame.gameWidth * 
+                         self.gameFrame.squareSize, self.gameFrame.gameHeight * self.gameFrame.squareSize)
         self.setWindowTitle('Snake')    
         self.show()
         
+        
 class SnakeFrame(QFrame):
+    """ Game frame where events occur and snake moves """
     
-    gameLoopTime = 30
+    gameLoopTime = 100
+    squareSize = 10
+    gameHeight = 50
+    gameWidth = 50
+    initialSnakePosition = [(25,22), (25,23), (25,24), (25,25)]
     
     def __init__(self):
         
         super().__init__()
         self.initFrame()
         
+        
     def initFrame(self):
         
         self.gameTimer = QTimer()
         self.snake = Snake()
+        
+        self.snake.setBodyPositions(self.initialSnakePosition)
+        
+        self.setStyleSheet("background-color:black")
+        
 
     def startGame(self):
         
@@ -53,12 +68,41 @@ class SnakeFrame(QFrame):
        
     
     def moveSnake(self):
-        """ Called every time timer times out """
+        """ Moves snake every time timer times out """
         
-        print("Timer")
+        self.snake.move()
+
+
+    def paintEvent(self, event):
+        
+        painter = QPainter(self)
+        self.drawSnake(event, painter)
+        
+        
+    def drawSnake(self, event, painter):
+        """ Draws the snake """
+        
+        pen = QPen()
+        pen.setWidth(1)
+        pen.setBrush(Qt.white)
+        pen.setCapStyle(Qt.SquareCap)
+        pen.setJoinStyle(Qt.MiterJoin)  
+        
+        brush = QBrush(Qt.white)
+        
+        painter.setPen(pen)
+        painter.setBrush(brush)
+        
+        for bodySquare in self.snake.bodyPositions:
+            """ Draw each individual square of snake """
+            
+            x = bodySquare[0] * self.squareSize
+            y = bodySquare[1] * self.squareSize
+            
+            painter.drawRect(x, y, self.squareSize, self.squareSize)
 
 if __name__ == '__main__':
     
     app = QApplication(sys.argv)
     window = SnakeWindow()
-    sys.exit(app.exec_())
+    app.exec_()
