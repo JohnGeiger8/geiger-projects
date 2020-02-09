2ba9459f26fdbcf4658318bca62dc634066f7bac
John Geiger <jqg5562@psu.edu>
2020-02-09 16:02:29 -0500

    Add Direction class and move function\n\nDirection class allows easy representation of movement of snake.  Move function will handle actually moving the snake

--- Snake/SnakeModel.py
+++ Snake/SnakeModel.py
@@ -1,8 +1,35 @@
 """
-Snake Application
+Created on Sat Feb 8 2020
 
-Author: John Geiger
+@author: JohnGeiger
 
-Created: February 8, 2020
+Snake Model
 
 """
+
+class Snake:
+    
+    bodyPositions = []
+    
+    def __init__(self):
+        
+        print("I'm a snake!")
+        
+    def move(self, direction):
+        
+        if direction == Direction.Left:
+            print("left")
+        if direction == Direction.Up:
+            print("Up")
+        if direction == Direction.Right:
+            print("Right")
+        if direction == Direction.Down:
+            print("Down")
+                
+     
+class Direction:
+    
+    Left = 0
+    Up = 1
+    Right = 2
+    Down = 3
