6f707c542bc3dbd1ebfdd84fcfddf9503cc51c0f
John Geiger <jqg5562@psu.edu>
2020-02-14 19:52:59 -0500

    Add test file for project unit tests

---
+++ JobWebscraper/test_JobWebScraper.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 16:59:15 2020
+
+@author: JohnGeiger
+
+Web Scraper Unit tests
+
+"""
+
+import JobWebScraper
+
+
+class TestWebScraper:
+    
+    def test_linkedIn_scrape(self):
+        assert True
