c1a52d713791b7f5eb70a6ec239c534bd2317251
John Geiger <jqg5562@psu.edu>
2020-02-16 10:08:30 -0500

    Update unit test to check equality of length of output

--- JobWebscraper/test_JobWebScraper.py
+++ JobWebscraper/test_JobWebScraper.py
@@ -9,10 +9,21 @@ Web Scraper Unit tests
 
 """
 
-import JobWebScraper
+from JobWebScraper import retrieve_LinkedIn_Jobs
 
 
 class TestWebScraper:
     
     def test_linkedIn_scrape(self):
-        assert True
+        
+        jobs, jobLinks, companies, locations = retrieve_LinkedIn_Jobs(
+                            jobDescription="Software engineer",
+                            jobLocation="Madison, Wisconsin, United States")
+        
+        size = len(jobs)
+        
+        if len(jobLinks) != size or len(companies) != size or len(locations) != size:
+            assert False
+        
+        else:
+            assert True
