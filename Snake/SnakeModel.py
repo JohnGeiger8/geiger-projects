00442da065634b72adc4e5e7d9fa60b79d24c22a
John Geiger <jqg5562@psu.edu>
2020-02-19 12:24:06 -0500

    Finalize code cleanup

--- Snake/SnakeModel.py
+++ Snake/SnakeModel.py
@@ -35,10 +35,12 @@ class Snake:
         self.bodyPositions = positions
 
     def headPosition(self):
-        
+
         return self.bodyPositions[len(self.bodyPositions) - 1]
 
     def move(self):
+
+        direction = self.direction
 
         # Allow snake to get longer if it's eating
         if self.isEating is True:
@@ -48,20 +50,20 @@ class Snake:
 
         headPosition = self.bodyPositions[len(self.bodyPositions) - 1]
 
-        if self.direction == Direction.Left or self.direction==Direction.Right:
+        if direction == Direction.Left or direction == Direction.Right:
             """ Horizontal move """
 
             # New position changes x by 1 or -1 for right or left movement
-            directionValue = 1 if self.direction == Direction.Right else -1
+            directionValue = 1 if direction == Direction.Right else -1
             newPosition = (headPosition[0] + directionValue, headPosition[1])
 
             self.bodyPositions.append(newPosition)
 
-        elif self.direction == Direction.Up or self.direction==Direction.Down:
+        elif direction == Direction.Up or direction==Direction.Down:
             """ Vertical move """
 
             # New position changes y value by 1 or -1 for down or up movement
-            directionValue = 1 if self.direction == Direction.Down else -1
+            directionValue = 1 if direction == Direction.Down else -1
             newPosition = (headPosition[0], headPosition[1] + directionValue)
 
             self.bodyPositions.append(newPosition)
